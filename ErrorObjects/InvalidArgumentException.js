c18b33c01eb552c56eeb91e89f3f330260e120b0
Sander Blue <william.a.blue@gmail.com>
2014-12-03 14:41:41 -0600

    add InvalidArgumentException and MissingArgumentException

---
+++ ErrorObjects/InvalidArgumentException.js
@@ -0,0 +1,8 @@
+function InvalidArgumentException(argument, method) {
+    this.message = 'Invalid argument ' + argument + ' passed to method ' + method;
+    this.name = 'InvalidArgumentException';
+
+    this.toString = function() {
+        return this.name + this.message
+    };
+}
